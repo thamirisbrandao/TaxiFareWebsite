922b97e909922c4302ec9995b65b975f114933e8
thamiris brandao <brandao.thamiris@gmail.com>
2022-03-15 16:34:02 -0300

    atualiza

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import datetime
 import pandas as pd
 import requests
-
+import numpy as np
 '''
 # TaxiFareModel front
 '''
@@ -18,6 +18,18 @@ lat_get = st.number_input('Insert latitude')
 dlon_get = st.number_input('Insert dropoff longitude')
 dlat_get = st.number_input('Insert dropoff latitude')
 pass_get = st.selectbox('Insert passenger count', list(range(1, 9)))
+
+@st.cache
+def get_map_data():
+
+    return pd.DataFrame(
+            np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+            columns=['lat', 'lon']
+        )
+
+df = get_map_data()
+
+st.map(df)
 
 url = f'https://taxifare.lewagon.ai/predict?pickup_datetime={d} {t}&pickup_longitude={lon_get}&pickup_latitude={lat_get}&dropoff_longitude={dlon_get}&dropoff_latitude={dlat_get}&passenger_count={pass_get}'
 
